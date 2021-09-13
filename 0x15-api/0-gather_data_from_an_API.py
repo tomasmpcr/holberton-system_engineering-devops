6f4541e821add833a10a42d9c3bf7836ed0d07a8
Tomas <mrtom@raspberrypi>
2021-09-13 14:57:07 +0100

    asd

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,27 @@ import sys
 
 if __name__ == "__main__":
     import requests
-    r_usuario = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])).json()
-    r_lista = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(sys.argv[1])).json()
-    
+    r_usuario = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+        ).json()
+    r_lista = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+         sys.argv[1])).json()
+
     lista = []
     number_of_done_task = 0
     total_number_of_task = 0
 
     for i in r_lista:
         total_number_of_task += 1
-        if i["completed"] == True:
+        if i["completed"] is True:
             number_of_done_task += 1
             lista.append(i["title"])
 
+    print("Employee {} is done with tasks({}/{}):".format(
+        r_usuario['name'],
+        number_of_done_task,
+        total_number_of_task))
 
-    print("Employee {} is done with tasks({}/{}):".format(r_usuario['name'], number_of_done_task, total_number_of_task))
     for lis in lista:
         print("\t{}".format(lis))
