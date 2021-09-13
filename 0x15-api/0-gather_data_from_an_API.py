c0cece100cb0879090e2c4f90ad6fdf0e79cd2d8
Tomas <mrtom@raspberrypi>
2021-09-13 14:52:03 +0100

    asd

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""DFGSS GSFG SDFGDSF """
+
+
+import sys
+
+if __name__ == "__main__":
+    import requests
+    r_usuario = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])).json()
+    r_lista = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(sys.argv[1])).json()
+    
+    lista = []
+    number_of_done_task = 0
+    total_number_of_task = 0
+
+    for i in r_lista:
+        total_number_of_task += 1
+        if i["completed"] == True:
+            number_of_done_task += 1
+            lista.append(i["title"])
+
+
+    print("Employee {} is done with tasks({}/{}):".format(r_usuario['name'], number_of_done_task, total_number_of_task))
+    for lis in lista:
+        print("\t{}".format(lis))
