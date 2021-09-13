0439fa97624ed7cc4e9a31a5ff0f6b72e0e38868
Tomas <mrtom@raspberrypi>
2021-09-13 15:08:19 +0100

    asd

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""DFGSS GSFG SDFGDSF """
+
+
+import sys
+
+if __name__ == "__main__":
+    import requests
+    r_usuario = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+        ).json()
+    r_lista = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+         sys.argv[1])).json()
+
+    user = r_usuario["username"]
+    f = open("{}.csv".format(sys.argv[1]), "w+")
+    
+    for i in r_lista:
+        f.write("\"{}\",\"{}\",\"{}\",\"{}\"\n".format(sys.argv[1], user, i["completed"], i["title"]))
+    f.close()
