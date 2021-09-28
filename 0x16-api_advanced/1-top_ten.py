f652f4a38510df715c610bc2171386a43b21e2c1
tshuna332 <tmmi333@gmail.com>
2021-09-28 20:59:13 +0000

    asd

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,8 @@ import requests
 def top_ten(subreddit):
     """ DASFGKSOGJNMSDFIKOGJSDFGJDS """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    conn = requests.get(url, headers={'User-agent': 'PIBE'})
+    headers = {'User-agent': 'PIBE'}
+    conn = requests.get(url, headers=headers, allow_redirects=False)
     if conn.status_code == 200:
         try:
             for i in range(10):
