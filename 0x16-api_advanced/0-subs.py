a6b9855e415fa14e7ad58738078bcb471fe5d777
tshuna332 <tmmi333@gmail.com>
2021-09-28 20:45:27 +0000

    fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-	ADSFJSDIFJISDKFGJSIDKGJDSIKGJNFDSIK
+    ADSFJSDIFJISDKFGJSIDKGJDSIKGJNFDSIK
 """
 
 import requests
@@ -8,12 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ DASFGKSOGJNMSDFIKOGJSDFGJDS """
-    conn = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                       headers={'User-agent': 'PIBE'})
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    conn = requests.get(url, headers={'User-agent': 'PIBE'})
     if conn.status_code == 200:
         try:
             return (conn.json()['data']['subscribers'])
         except KeyError:
             return 0
     return 0
-
