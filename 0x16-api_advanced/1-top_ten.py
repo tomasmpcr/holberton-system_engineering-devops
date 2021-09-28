0ca6ae5691327bc962d03026c136fa8f1570a446
tshuna332 <tmmi333@gmail.com>
2021-09-28 20:56:16 +0000

    asd

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    ADSFJSDIFJISDKFGJSIDKGJDSIKGJNFDSIK
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ DASFGKSOGJNMSDFIKOGJSDFGJDS """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    conn = requests.get(url, headers={'User-agent': 'PIBE'})
+    if conn.status_code == 200:
+        try:
+            for i in range(10):
+                print(conn.json()['data']['children'][i]['data']['title'])
+            return
+        except IndexError:
+            return None
+    return None
